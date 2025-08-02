d551b03cfdb4dc47e51b43182f8c6c34ceef1006
miruky <h.miyoki@gmail.com>
2025-08-02 21:03:09 +0900

    ライブラリの公開APIを集約

---
+++ src/lib/index.ts
@@ -0,0 +1,19 @@
+export { FORM_LABELS, verbs } from './data/verbs';
+export { misuseItems } from './data/misuse';
+export {
+  buildConversionQuestion,
+  buildMisuseQuestion,
+  conversionId,
+  createRng,
+  shuffle,
+} from './quiz';
+export { accuracy, deserialize, emptyProgress, record, serialize, weakIds } from './progress';
+export type { Progress } from './progress';
+export type {
+  ConversionQuestion,
+  KeigoForm,
+  MisuseItem,
+  MisuseQuestion,
+  Question,
+  VerbEntry,
+} from './types';
